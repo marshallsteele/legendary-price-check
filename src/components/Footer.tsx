a9efb3c99ba3479554dc9c8389d11df952522ba6
Marshall Steele <marshallsteele123@hotmail.com>
2024-10-07 10:09:14 -0400

    Added disclaimer and title as per arenanet legal requirements

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,10 @@ import './Footer.css'
 function Footer () {
     return (
         <div className="footerContainer">
-            <p>
-            This is where the disclaimer goes<br /> 
-            All art and stuff is the property of gw<br /> 
-            Somewhere theres a picture of the GW2 logo I think
+            <p className='footerDisclaimer'>
+            This is an unofficial site that is not affiliated with or endorsed by ArenaNet.
+            © ArenaNet LLC. All rights reserved. NCSOFT, ArenaNet, Guild Wars, Guild Wars 2, GW2, Guild Wars 2: Heart of Thorns, Guild Wars 2: Path of Fire, Guild Wars 2: End of Dragons, and Guild Wars 2: Secrets of the Obscure and all associated logos, designs, and composite marks are trademarks or registered trademarks of NCSOFT Corporation.
+            All other trademarks are the property of their respective owners.
             </p>
         </div>
     )
